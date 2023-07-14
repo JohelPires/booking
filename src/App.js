570ce98eec08c0c18bc424068b057b6fa3f12a3d
JohelPires <johelpires@gmail.com>
2023-07-14 09:45:27 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import { useState } from 'react'
 import './App.css'
+import { HiMenu } from 'react-icons/hi'
 
 function App() {
     const [party, setParty] = useState(3)
+    const [hour, setHour] = useState('8 p.m.')
+
+    function handleHour(s) {
+        let h = parseInt(hour.split(' ')[0])
+
+        if (s === '+') {
+            if (h < 12) {
+                h += 1
+            }
+        } else {
+            if (h > 6) {
+                h -= 1
+            }
+        }
+        setHour(`${h} p.m.`)
+        console.log(h)
+    }
 
     return (
-        <main className="App">
-            <nav>menu</nav>
+        <main className='App'>
+            <nav>
+                <HiMenu />
+            </nav>
             <header>
                 <img
-                    src="https://bookya.com.au/wp-content/uploads/2022/05/hitesh-dewasi-5JwBbnyZzfc-unsplash-1.jpg"
-                    alt=""
+                    src='https://bookya.com.au/wp-content/uploads/2022/05/hitesh-dewasi-5JwBbnyZzfc-unsplash-1.jpg'
+                    alt=''
                 />
             </header>
             <section>
                 <h1>Reserve a table</h1>
-                <div className="control">
+                <div className='control'>
                     <p>Party size</p>
-                    <div className="control">
+                    <div className='control'>
                         <button
                             onClick={() => {
                                 setParty((prev) => (prev > 1 ? prev - 1 : prev))
@@ -27,7 +47,7 @@ function App() {
                         </button>
                         <p>
                             {' '}
-                            <input style={{ width: '30px' }} type="text" value={party} />{' '}
+                            <input style={{ width: '30px' }} type='text' value={party} />{' '}
                         </p>
                         <button
                             onClick={() => {
@@ -38,18 +58,24 @@ function App() {
                         </button>
                     </div>
                 </div>
-                <div className="control">
+                <div className='control'>
                     <p>Date</p>
 
-                    <input type="date" />
+                    <input type='date' />
                 </div>
-                <div className="control">
+                <div className='control'>
                     <p>Hour</p>
-
-                    <p>3</p>
+                    <div className='control'>
+                        <button onClick={() => handleHour('-')}>-</button>
+                        <p>
+                            {' '}
+                            <input style={{ width: '50px' }} type='text' value={hour} />{' '}
+                        </p>
+                        <button onClick={() => handleHour('+')}>+</button>
+                    </div>
                 </div>
-                <div className="control">
-                    <button style={{ margin: 'auto' }}>Confirm</button>
+                <div className='control-button'>
+                    <button className='confirm'>Confirm</button>
                 </div>
             </section>
         </main>
